27d7f5a2cdbff60e52961b8bfd3ac91cddbbf207
Swiftsoftware143 <152040982+Swiftsoftware143@users.noreply.github.com>
2026-05-12 00:49:07 -0400

    Add Personal Websites route to navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Clients from "@/pages/Clients";
 import ClientDetail from "@/pages/ClientDetail";
 import EmbedCodePage from "@/pages/EmbedCodePage";
 import Settings from "@/pages/Settings";
+import PersonalWebsites from "@/pages/PersonalWebsites";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/clients/:id" element={<ClientDetail />} />
             <Route path="/embed" element={<EmbedCodePage />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/personal-websites" element={<PersonalWebsites />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
